9d87553bf152637d8a6aca3f5271e0b972dd7df4
wachiravitlouis20 <louiskungking@gmail.com>
2025-07-22 20:15:32 +0700

    Update Data_Cleaning_Preparation.sql

--- Data_Cleaning_Preparation.sql
+++ Data_Cleaning_Preparation.sql
@@ -29,6 +29,46 @@ from
 where
     order_status not in ('unavailable', 'canceled');
 
--- ## 
-select distinct payment_type 
-from olist_order_payments_dataset;
+-- ## Check Null Dupicate format and Clean olist_order_item_dataset
+-- ## Check Null
+select 
+	count(*)
+from 
+	olist_order_items_dataset
+where order_id is null
+	  or product_id is null
+	  or price is null;
+
+-- ## Check Dupicate
+select
+	order_id,
+    order_item_id,
+    product_id,
+    count(*) as check_dupicate
+from olist_order_items_dataset
+	group by
+	order_id,
+    order_item_id,
+    product_id
+   	having
+   	count(*) > 1;
+-- ## Check logical error (Price and Freight Shoud not below 0)
+select 
+	price,
+	freight_value
+from olist_order_items_dataset
+where price <= 0 or freight_value <= 0;
+
+--## Creat View table with clean data
+create or replace view avw_order_item_dataset as
+	select
+	order_id,
+	order_item_id,
+	product_id,
+	seller_id,
+	shipping_limit_date::timestamp as ship_limit_date,
+	price,
+	freight_value
+from
+	olist_order_items_dataset;
+
