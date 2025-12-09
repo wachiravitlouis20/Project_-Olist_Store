22e31133089125260a215a77b9cd6782ee43a41b
wachiravitlouis20 <louiskungking@gmail.com>
2025-12-09 08:17:16 +0700

    Update Base Layer.sql

--- Base Layer.sql
+++ Base Layer.sql
@@ -1,30 +1,30 @@
--- BASE LAYER: one row per order
-CREATE OR REPLACE VIEW `steam-form-479809-a3.OlistProject.vw_kpi_base` AS
-WITH item_rollup AS (
-  SELECT
+CREATE OR REPLACE VIEW `steam-form-479809-a3.OlistProject.base_layer` AS
+WITH item_rollup AS(
+  SELECT 
     oi.order_id,
-    SUM(oi.price)         AS price_sum,
-    SUM(oi.freight_value) AS freight_sum
-  FROM `steam-form-479809-a3.OlistProject.avw_order_item_dataset` AS oi
-  GROUP BY oi.order_id
+    SUM(oi.price) AS total_price,
+    SUM(oi.freight_value) AS total_freight_value
+  FROM `steam-form-479809-a3.OlistProject.avw_order_item_dataset` oi
+  GROUP BY oi.order_id # Group by order id ไม่ซ้ำกัน
 ),
-payment_rollup AS (
+payment_rollup AS(
   SELECT
     p.order_id,
-    SUM(p.payment_value) AS paid_sum
-  FROM `steam-form-479809-a3.OlistProject.vw_olist_payment_cleaned` AS p
-  GROUP BY p.order_id
+    SUM(p.payment_value) AS total_payment_value
+  FROM `steam-form-479809-a3.OlistProject.vw_olist_payment_cleaned` p
+  GROUP BY p.order_id # Group by order id ไม่ซ้ำกัน
 )
-SELECT
+SELECT  
   o.order_id,
   o.customer_id,
-  DATE_TRUNC(DATE(o.purchase_timestamp), MONTH) AS order_month,
+  DATETIME_TRUNC(purchase_timestamp, MONTH) AS order_month,
   o.order_status,
-  ir.price_sum,
-  ir.freight_sum,
-  COALESCE(ir.price_sum, 0) + COALESCE(ir.freight_sum, 0) AS gmv_defined,
-  pr.paid_sum
-FROM `steam-form-479809-a3.OlistProject.vw_orders_cleaned` AS o
-LEFT JOIN item_rollup   AS ir ON ir.order_id = o.order_id
-LEFT JOIN payment_rollup AS pr ON pr.order_id = o.order_id
-WHERE o.order_status IN ('approved','invoiced','shipped','delivered');
+  pr.total_payment_value,
+  ir.total_price,
+  ir.total_freight_value,
+  COALESCE(ir.total_price,0) + COALESCE(ir.total_freight_value, 0) AS GMV_defined
+FROM `steam-form-479809-a3.OlistProject.vw_orders_cleaned` o
+LEFT JOIN payment_rollup pr ON o.order_id = pr.order_id
+LEFT JOIN item_rollup ir ON o.order_id = ir.order_id
+WHERE o.order_status IN ('delivered', 'shipped', 'approved','invoiced')
+ORDER BY o.order_id
